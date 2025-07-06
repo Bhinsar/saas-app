edcd6d6675752dedda72b3f7a3c3a074d289d534
Bhinsar Jagdish <bhinsarjagdish2003@gmail.com>
2025-07-06 14:11:24 +0530

    Create the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
-import { Button } from '@/components/ui/button'
+import CompanionCards from "@/components/CompanionCards";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
-  return (
-    <>
-    <div className='text-2xl font-bold'>Page</div>
-    <Button>Lets get Started</Button>
-    </>
-  )
+    return (
+        <main>
+            <h1>Popular Companions</h1>
+            <section className={'home-section'}>
+                <CompanionCards
+                    id="1"
+                    subject="science"
+                    name="Neura the Brainy Explorer"
+                    topic="Neural Network of the Brain"
+                    duration={45}
+                    color="#E5D0FF"
+                />
+                <CompanionCards
+                    id="2"
+                    subject="maths"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    duration={30}
+                    color="#FFDA6E"
+                />
+                <CompanionCards
+                    id="3"
+                    subject="language"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature"
+                    duration={30}
+                    color="#BDE7FF"
+                />
+            </section>
+            <section className={'home-section'}>
+                <CompanionsList
+                    title="Recent Completed Companions"
+                    companions={recentSessions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA/>
+            </section>
+        </main>
+    )
 }
 
 export default Page
